25dfec39d76b7517fcc721b494ae8bb06ddb1f95
Gideon Sireling <code@accursoft.com>
2014-11-27 12:23:13 +0200

    move response to data file

--- Main.hs
+++ Main.hs
@@ -3,9 +3,11 @@ import Data.Char
 import System.IO
 import System.IO.Error
 import Network
+import Paths_server
 
 main = do sock <- listenOn $ PortNumber 8080
+          response <- getDataFileName "response" >>= readFile
           forever $ do (handle,_,_) <- accept sock
                        request <- hGetContents handle
-                       when (any (null . dropWhile isSpace) (lines request)) $ void $ tryIOError $ hPutStr handle "HTTP/1.1 200 OK\r\n\r\nWelcome"
+                       when (any (null . dropWhile isSpace) (lines request)) $ void $ tryIOError $ hPutStr handle response
                        hClose handle
