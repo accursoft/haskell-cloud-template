45850d2e79a240ce7ed60abcbb2eec3c498de347
Gideon Sireling <code@accursoft.com>
2017-08-17 13:03:48 +0300

    handle IO exceptions

--- Main.hs
+++ Main.hs
@@ -3,11 +3,13 @@ import Data.Char
 import System.IO
 import System.IO.Error
 import Network
+import Control.Exception
 import Paths_server
 
 main = do sock <- listenOn $ PortNumber 8080
           response <- getDataFileName "response" >>= readFile
           forever $ do (handle,_,_) <- accept sock
-                       request <- hGetContents handle
-                       when (any (null . dropWhile isSpace) (lines request)) $ void $ tryIOError $ hPutStr handle response
+                       catch (do request <- hGetContents handle
+                                 when (any (null . dropWhile isSpace) (lines request)) $ void $ tryIOError $ hPutStr handle response)
+                             (\e -> putStrLn $ show (e::IOException))
                        hClose handle
